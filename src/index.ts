489780dde7cdf3a691c9d70015b03d09dc7acb96
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-09-16 14:25:25 +0300

    awd

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-
  const axiosInstance = axios.create({
     baseURL:process.env.NODE_ENV==="development" ? "http://localhost:6777/" : "https://ocserver.failean.com/",
     withCredentials: true,
